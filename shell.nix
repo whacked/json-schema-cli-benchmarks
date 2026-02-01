f8033f277baa3355389e414b0dc87ae131d3591a
whacked <whacked@users.noreply.github.com>
2026-02-02 01:42:57 +0800

    update dirschema version

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ let
   }) { inherit pkgs; };
 
   # remote:
-  dirschema = (builtins.getFlake "github:whacked/dirschema/256f23e").packages.${pkgs.system}.default;
+  dirschema = (builtins.getFlake "github:whacked/dirschema/e5b81b8").packages.${pkgs.system}.default;
   # local:
   # dirschema = (builtins.getFlake "/path/to/dirschema").packages.${pkgs.system}.default;
 
